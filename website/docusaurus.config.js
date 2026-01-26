29fd4aadee47657aa5236dd68372a23712b884b9
politiboop <basketball469@msn.com>
2026-01-26 14:19:32 -0700

    Fix baseUrl for Vercel deployment
    Change baseUrl from /controversial-trump/ to / for Vercel hosting

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://politiboop.github.io',
+  url: 'https://controversial-trump.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/controversial-trump/',
+  // For Vercel and most hosting, this should be '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
